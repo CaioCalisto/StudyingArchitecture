ca43f01a1ecf85136801b0db33be6fe549c024eb
Caio Cesar Calisto <caio.calisto@hitachivantara.com>
2020-11-01 14:51:12 +0000

    Improving query

--- CarHire/Registration/Contoso.Registration.API/Contoso.Registration.Application/Queries/VehiclesQueries.cs
+++ CarHire/Registration/Contoso.Registration.API/Contoso.Registration.Application/Queries/VehiclesQueries.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 using AutoMapper;
 using Contoso.Registration.Infrastructure.Database;
 using Microsoft.Azure.Cosmos.Table;
@@ -29,48 +32,15 @@ namespace Contoso.Registration.Application.Queries
         public IEnumerable<Model.Vehicle> Find(Model.Vehicle vehicle)
         {
             IQueryable<TableEntityAdapter<Infrastructure.Model.Vehicle>> query = this.databaseQueries.GetQuery<Infrastructure.Model.Vehicle>();
-            if (!string.IsNullOrEmpty(vehicle.Brand))
-            {
-                query = query.Where(c => c.OriginalEntity.Brand == vehicle.Brand);
-            }
 
-            if (!string.IsNullOrEmpty(vehicle.Name))
-            {
-                query = query.Where(c => c.OriginalEntity.Name == vehicle.Name);
-            }
+            List<TableEntityAdapter<Infrastructure.Model.Vehicle>> result = query
+                .Where(q => (string.IsNullOrEmpty(vehicle.Brand) || q.OriginalEntity.Brand == vehicle.Brand) &&
+                (string.IsNullOrEmpty(vehicle.Name) || q.OriginalEntity.Name == vehicle.Name) &&
+                (string.IsNullOrEmpty(vehicle.Category) || q.OriginalEntity.Category == vehicle.Category) &&
+                (string.IsNullOrEmpty(vehicle.Transmission) || q.OriginalEntity.Transmission == vehicle.Transmission))
+                .ToList();
 
-            if (!string.IsNullOrEmpty(vehicle.Transmission))
-            {
-                query = query.Where(c => c.OriginalEntity.Transmission == vehicle.Transmission);
-            }
-
-            if (!string.IsNullOrEmpty(vehicle.Category))
-            {
-                query = query.Where(c => c.OriginalEntity.Category == vehicle.Category);
-            }
-
-            if (vehicle.Passengers.HasValue)
-            {
-                query = query.Where(c => c.OriginalEntity.Passengers == vehicle.Passengers);
-            }
-
-            if (vehicle.Doors.HasValue)
-            {
-                query = query.Where(c => c.OriginalEntity.Doors == vehicle.Doors);
-            }
-
-            if (vehicle.Consume.HasValue)
-            {
-                query = query.Where(c => c.OriginalEntity.Consume == vehicle.Consume);
-            }
-
-            if (vehicle.Emission.HasValue)
-            {
-                query = query.Where(c => c.OriginalEntity.Emission == vehicle.Emission);
-            }
-
-            IEnumerable<Infrastructure.Model.Vehicle> result = query.ToList().Select(c => c.OriginalEntity);
-            return this.mapper.Map<IEnumerable<Infrastructure.Model.Vehicle>, IEnumerable<Model.Vehicle>>(result);
+            return this.mapper.Map<IEnumerable<Infrastructure.Model.Vehicle>, IEnumerable<Model.Vehicle>>(result.Select(c => c.OriginalEntity));
         }
     }
 }
