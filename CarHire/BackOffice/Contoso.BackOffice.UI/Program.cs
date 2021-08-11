bdd1339a0e41fd22961f0c57a323b860de1bef1b
Caio Cesar Calisto <caiocesarcalisto@gmail.com>
2021-08-11 22:27:24 +0100

    Added Blazorise

--- CarHire/BackOffice/Contoso.BackOffice.UI/Program.cs
+++ CarHire/BackOffice/Contoso.BackOffice.UI/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace Contoso.BackOffice.UI
 {
@@ -15,6 +18,14 @@ namespace Contoso.BackOffice.UI
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services
+                .AddBlazorise( options =>
+                {
+                    options.ChangeTextOnKeyPress = true;
+                } )
+                .AddBootstrapProviders()
+                .AddFontAwesomeIcons();
+            
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
